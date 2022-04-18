7ef7b7d98deeb68a885fd59997ab3ce9479ba775
unknown <elouan112@gmail.com>
2022-04-18 02:00:12 -0400

    finished full 1bit adder

--- src/ca/vcircuits/circuits/FullAdder1B.java
+++ src/ca/vcircuits/circuits/FullAdder1B.java
@@ -44,6 +44,30 @@ public class FullAdder1B {
 
 
     private void update() {
-        
+        addAB.setA(a.getStatus());
+        addAB.setB(b.getStatus());
+
+        addCarry.setA(addAB.isOn());
+        addCarry.setB(carryIn.getStatus());
+
+        abCarry.setA(a.getStatus());
+        abCarry.setB(b.getStatus());
+
+        sumAndCarry.setA(addAB.isOn());
+        sumAndCarry.setB(carryIn.getStatus());
+
+        carryOrCarry.setA(abCarry.isOn());
+        carryOrCarry.setB(sumAndCarry.isOn());
+
+        sum.setStatus(addCarry.isOn());
+        carryOut.setStatus(carryOrCarry.isOn());
+    }
+
+    public boolean getSum() {
+        return sum.getStatus();
+    }
+
+    public boolean getCarryOut() {
+        return sum.getStatus();
     }
 }
